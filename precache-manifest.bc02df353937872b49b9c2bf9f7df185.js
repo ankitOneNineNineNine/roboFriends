0882c279964029d1329460161cf6fc612c3a85a0
ankitOneNineNineNine <pradhanankit12@gmail.com>
2020-11-26 16:11:15 +0545

    Updates

---
+++ precache-manifest.bc02df353937872b49b9c2bf9f7df185.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca5ce50aa68335bae61524201d5ecd2b",
+    "url": "/roboFriends/index.html"
+  },
+  {
+    "revision": "3148a004f903ec72e3ba",
+    "url": "/roboFriends/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "b4904ec811d30be063a0",
+    "url": "/roboFriends/static/css/main.efa632ac.chunk.css"
+  },
+  {
+    "revision": "3148a004f903ec72e3ba",
+    "url": "/roboFriends/static/js/2.b0f4c8b7.chunk.js"
+  },
+  {
+    "revision": "b4904ec811d30be063a0",
+    "url": "/roboFriends/static/js/main.b29c093a.chunk.js"
+  },
+  {
+    "revision": "0d6209f39b04744aca3d",
+    "url": "/roboFriends/static/js/runtime~main.0e8c0065.js"
+  },
+  {
+    "revision": "bcfa3a4184aa5b37b2a4033f6ec15325",
+    "url": "/roboFriends/static/media/Team 401.bcfa3a41.woff"
+  }
+]);
